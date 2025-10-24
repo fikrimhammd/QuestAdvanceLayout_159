c1bd7016e48c5c6d505b5c4025a434606037ca60
fikrimhammd <fikri.kdi60@gmail.com>
2025-10-24 10:20:53 +0700

    Mengubah MainActivity

--- app/src/main/java/com/example/questadvancelayout_159/MainActivity.kt
+++ app/src/main/java/com/example/questadvancelayout_159/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestAdvanceLayout_159Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    ActivitasPertama(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
